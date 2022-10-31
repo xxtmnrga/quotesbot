a5186a7c9a45d31fe2d1d914509f638fe4380aa7
xxtmnrga <xxtmnrga@outlook.com>
2022-10-31 14:21:33 +0800

    Update yande.py

--- quotesbot/spiders/yande.py
+++ quotesbot/spiders/yande.py
@@ -12,9 +12,8 @@ class ToScrapeSpiderXPath(scrapy.Spider):
         for quote in response.xpath('//a[@class="thumb"]'):
             yield {
                 'imgurl': quote.xpath('./img/@src').extract_first(),
-                image_urls =quote.xpath('./img/@src').extract_first(),
             }
-
+        image_urls = scrapy.Field()
         next_page_url = response.xpath('//a[@class="next_page"]/@href').extract_first()
         if next_page_url is not None:
             yield scrapy.Request(response.urljoin(next_page_url))
